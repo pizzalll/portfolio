f3be8b470e9e50f4678d365b1b9417804b55c186
pizzalll <58722972+pizzalll@users.noreply.github.com>
2020-03-04 19:14:46 +0900

    Add files via upload

---
+++ shopping-mall/js/main.js
@@ -0,0 +1,75 @@
+// $(document).ready(function(){
+//     $('.slider').bxSlider({
+//         mode: 'fade',
+//         auto: true,
+//         pager: true,
+//         pause: 4000,
+//     });
+//   });
+
+// $(document).ready(function(){
+//     $('.bxslider').bxSlider({
+//         auto: true,
+//         pause: 6000,
+//         minSlides: 1,
+//         maxSlides: 3,
+//         slideWidth: 300,
+//         pager: false,
+//         controls: false,
+//         slideMargin: 150,
+//         speed: 700,
+//     });
+// });
+
+// $('.slider').imagesLoaded(function(){
+//     $(document).ready(function(){
+//         $('.slider').bxSlider({
+//             mode: 'fade',
+//             auto: true,
+//             pager: true,
+//             pause: 4000,
+//         });
+//     });
+// });
+
+
+// $('.bxslider').imagesLoaded(function(){
+//     $(document).ready(function(){
+//         $('.bxslider').bxSlider({
+//             auto: true,
+//             pause: 6000,
+//             minSlides: 1,
+//             maxSlides: 3,
+//             slideWidth: 300,
+//             pager: false,
+//             controls: false,
+//             slideMargin: 150,
+//             speed: 700,
+//         });
+//     });
+// });
+
+$(function(){
+    $('.slider').imagesLoaded(function(){
+        $('.slider').bxSlider({
+            mode: 'fade',
+            auto: true,
+            pager: true,
+            pause: 4000,
+        });
+    });
+
+    $('.bxslider').imagesLoaded(function(){
+        $('.bxslider').bxSlider({
+            auto: true,
+            pause: 6000,
+            minSlides: 1,
+            maxSlides: 3,
+            slideWidth: 300,
+            pager: false,
+            controls: false,
+            slideMargin: 150,
+            speed: 700,
+        });
+    });
+});
